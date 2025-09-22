2a0e734a52d7177bda73a1d521cfb3b3629c44b2
akin.delu <akindelu@MacBookPro.lan>
2025-09-22 15:13:56 -0600

    fix: mock api integration with chat page

--- backend/app/api/routes/chat.py
+++ backend/app/api/routes/chat.py
@@ -7,14 +7,6 @@ from pydantic import BaseModel
 
 from app.api.deps import CurrentUser, SessionDep
 from app.models.common import Message
-from app.models.course import (
-    Course,
-    CourseCreate,
-    CoursePublic,
-    CoursesPublic,
-    CourseUpdate,
-)
-from app.models.document import Document, DocumentPublic
 
 class Chat(BaseModel):
     chat_id: str
@@ -29,14 +21,10 @@ def list_chat(session: SessionDep, current_user: CurrentUser, id: uuid.UUID, cur
     """
     Get chat history for a course.
     """
-    statement = (
-        select(Course).where(Course.id == id).options(selectinload(Course.documents))
-    )
-    course = session.exec(statement).first()
-
-    if not course:
-        raise HTTPException(status_code=404, detail="Course not found")
-    if not current_user.is_superuser and (course.owner_id != current_user.id):
-        raise HTTPException(status_code=400, detail="Not enough permissions")
-
-    return course
+    
+@router.get("/{course_id}", response_model=Chat)
+def send_chat(session: SessionDep, current_user: CurrentUser, id: uuid.UUID) -> Any:
+    """
+    Send chat for a course.
+    """
+    
