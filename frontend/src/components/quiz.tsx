a1ea9b252269e0ed405e6b2d3b8aa07dc6c07ab5
Michael Gichia <gichuru.gichia@gmail.com>
2025-09-26 21:15:20 +0300

    Add checkbox.

--- frontend/src/components/quiz.tsx
+++ frontend/src/components/quiz.tsx
@@ -1,13 +1,17 @@
-
 import React from 'react'
 
-import { CourseWithDocuments } from '@/client'
-import { getQuizzes } from '@/actions/quizzes'
+import {CourseWithDocuments} from '@/client'
+import {getQuizzes} from '@/actions/quizzes'
 import ErrorBox from '@/components/ui/ErrorBox'
+import {Checkbox} from '@/components/ui/checkbox'
+import {Label} from '@/components/ui/label'
 
-
-export default async function QuizComponent({ course }: { course: CourseWithDocuments }) {
-    const result = await getQuizzes(course.id)
+export default async function QuizComponent({
+  course,
+}: {
+  course: CourseWithDocuments
+}) {
+  const result = await getQuizzes(course.id)
 
   if (!result.ok) {
     return <ErrorBox error={result.error} />
@@ -16,15 +20,21 @@ export default async function QuizComponent({ course }: { course: CourseWithDocu
   return (
     <div className='h-full flex flex-col'>
       {result.data.map((quiz, idx) => (
-        <div>
-          <p> {idx + 1}. {quiz.quiz_text}</p>
+        <div className='flex flex-col gap-6'>
+          <p>
+            {' '}
+            {idx + 1}. {quiz.quiz_text}
+          </p>
           <ul>
             {quiz.choices.map((choice) => (
-              <li key={choice.id}>{choice.text}</li>
+              <div className='flex items-center gap-3 mb-4' key={choice.id}>
+                <Checkbox id='terms' />
+                <Label htmlFor='terms'>{choice.text}</Label>
+              </div>
             ))}
           </ul>
         </div>
       ))}
     </div>
-    )
-  }
+  )
+}
